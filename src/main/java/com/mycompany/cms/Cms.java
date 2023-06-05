c68d632e5047822f3c8c0951585023b6a1dd0bf1
ali m alghanay <a.al-ghanay@uot.edu.ly>
2023-06-05 19:42:32 +0200

    first commit

---
+++ src/main/java/com/mycompany/cms/Cms.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cms;
+
+/**
+ *
+ * @author HP
+ */
+public class Cms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Index test1 = new Index();
+        test1.veiw();
+    }
+}
