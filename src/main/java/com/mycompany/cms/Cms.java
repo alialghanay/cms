86de475d5ba3946cd2fdff269e27aa4afd5eaf1f
Ahmed-Elmahjoub <ahmedelmahjoub0@gmail.com>
2023-06-06 12:38:16 +0200

    Ahmed: completed Agencies

--- src/main/java/com/mycompany/cms/Cms.java
+++ src/main/java/com/mycompany/cms/Cms.java
@@ -13,6 +13,8 @@ public class Cms {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Index test1 = new Index();
+        Agencies test2 = new Agencies();
+        test2.veiw();
         test1.veiw();
     }
 }
